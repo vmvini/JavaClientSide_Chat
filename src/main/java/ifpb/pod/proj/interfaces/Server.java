bdc08c60a116e8063f10cf242d90048ca5f31b2a
Marcus Vinicius <vmvini@hotmail.com>
2016-05-07 20:53:58 -0300

    atualizando interface Server

--- src/main/java/ifpb/pod/proj/interfaces/Server.java
+++ src/main/java/ifpb/pod/proj/interfaces/Server.java
@@ -1,5 +1,6 @@
 package ifpb.pod.proj.interfaces;
 
+import javax.naming.AuthenticationException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
@@ -12,7 +13,8 @@ public interface Server extends Remote {
 
     String cadastrarUsuario(String nome, String email, String senha) throws RemoteException;
 
-    void inscreverGrupo(Usuario user, String grupoId) throws RemoteException;
+    void inscreverGrupo(Usuario user, String grupoId, String sessionToken) throws RemoteException, AuthenticationException;
 
-    void escreverMensagem(String usrEmail, String grupoId, String conteudo) throws RemoteException;
+    void escreverMensagem(String usrEmail, String grupoId, String conteudo, String sessionToken) throws RemoteException, AuthenticationException;
+
 }
