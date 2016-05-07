acadf1145a97a3af1f59365ae242be729dbd5da4
Marcus Vinicius <vmvini@hotmail.com>
2016-05-07 14:46:11 -0300

    ajustes na implementação de Usuario remoto

--- src/main/java/ifpb/pod/proj/clientside/UserImpl.java
+++ src/main/java/ifpb/pod/proj/clientside/UserImpl.java
@@ -8,15 +8,18 @@ import java.rmi.server.UnicastRemoteObject;
 /**
  * Created by vmvini on 07/05/16.
  */
-public class UserImpl implements Usuario {
+public class UserImpl extends UnicastRemoteObject implements Usuario {
 
     private String email;
     private String senha;
+    private UserSideClientJava clientJava;
 
 
-    public UserImpl(String email, String senha){
+    public UserImpl(String email, String senha, UserSideClientJava clientJava) throws RemoteException{
+        super();
         this.email = email;
         this.senha = senha;
+        this.clientJava = clientJava;
     }
 
     public String getEmail() throws RemoteException{
@@ -26,7 +29,9 @@ public class UserImpl implements Usuario {
         return senha;
     }
 
+    //str é token de notificação de mensagens pendentes
     public boolean notificar(String str) throws RemoteException{
-        return false;
+        return clientJava.sendNotificationToUser(str);
+
     }
 }
