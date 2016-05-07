878a2829b1b70b121b95f8e5d1e05a117b3d5cdd
Marcus Vinicius <vmvini@hotmail.com>
2016-05-07 16:19:41 -0300

    Main adicionado

---
+++ src/main/java/ifpb/pod/proj/clientside/Main.java
@@ -0,0 +1,16 @@
+package ifpb.pod.proj.clientside;
+
+/**
+ * Created by vmvini on 07/05/16.
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+        UserSideClientJava clientJava = new UserSideClientJava();
+
+        clientJava.listenConnection();
+
+    }
+
+}
